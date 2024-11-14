358fb8e56c8ab680c4c80ed29375f653e4badaf5
Urs Schmidt <ufogreeks@gmail.com>
2024-11-14 15:57:36 +0100

    fix: calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: username <your@email.com>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 10:29:52 by username          #+#    #+#             */
-/*   Updated: 2024/11/13 11:15:26 by username         ###   ########.fr       */
+/*   Updated: 2024/11/14 15:57:23 by uschmidt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,13 @@ void	*ft_calloc(size_t nmemb, size_t size)
 		if ((int)nmemb * (int)size < 0)
 				return (NULL);
 		int	*ptr;
+		if (nmemb == 0 || size == 0)
+		{
+				ptr = (int *)malloc(sizeof(int));
+						return ((void *)ptr);
+		}
 		ptr = (int *)malloc(nmemb * size);
 		if (!ptr)
 				return (NULL);
-		else if (nmemb == 0 || size == 0)
-		{
-				ptr = (int *)malloc(sizeof(int));
-				if (ptr)
-						return ((void *)ptr);
-				else
-						return (NULL);
-		}
 		return (ft_memset(ptr, 0, nmemb * size));
 }
